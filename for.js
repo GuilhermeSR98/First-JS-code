583b7c247ed8036360bf9dca5957e012fb4da9bd
gui <guigareisbr@gmail.com>
2021-06-20 11:56:29 -0300

    Working with for

---
+++ for.js
@@ -0,0 +1,65 @@
+console.log(`Working with arrays`);
+
+const destinations = new Array(
+    `Salvador`, 
+    `Bahia`, 
+    `Rio de Janeiro`
+); 
+destinations.push(`Gramado`); // Adding value to array
+console.log(`Avaliable destinations: ${destinations}`);
+
+//Conditionals
+
+const age = 20;
+const accompanied = true;
+let passage = false;
+let cont = 0;
+let destinyExists = false;
+const destiny = '';
+
+if (age >= 18) { 
+     console.log(`Destiny ${destinations[1]} bought.`);
+     destinations.splice(1,1); //removing value from array
+     passage = true;
+}
+else {
+    console.log('Client under age.');
+    if(accompanied == true) {
+       console.log(`Client accompanied, destiny ${destinations[1]} bought.`);
+       destinations.splice(1,1); //removing value from array
+       passage = true;
+    } else {
+        console.log(`Client unattended, canno't buy.`);
+        passage = false;
+    }
+}
+if(passage == true || accompanied == true) {
+   console.log('boarded passenger');
+}
+
+while (cont < destinations.length && destinyExists == false) {
+    if (destiny == destinations[cont]) {
+        destinyExists = true;
+    }
+    cont ++;
+}
+
+for (let i = 0; destinations.length < i; i++) {
+    if (destiny == destinations[i]) {
+        destinyExists = true;
+    }
+}
+
+if (destinyExists == true) {
+    console.log(`Destiny ${destiny} exists.`);
+}
+else {
+    console.log(`Destiny ${destiny} isn't avaliable.`);
+}
+
+for (let c = 0; destinations.length < c; c++) {
+    if (destiny == destinations[c]) {
+        destinyExists = true;
+    }
+}
+ 
