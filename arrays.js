fad6b865c3408171e1d79e889423e93c02a5f721
gui <guigareisbr@gmail.com>
2021-06-20 09:53:26 -0300

    Working with arrays

--- arrays.js
+++ arrays.js
@@ -1,12 +1,36 @@
  console.log(`Working with arrays`);
 
  const destinations = new Array(
-     `Salvador`,
-     `Bahia`,
+     `Salvador`, 
+     `Bahia`, 
      `Rio de Janeiro`
  ); 
-destinations.push(`Gramado`); // Adding value to array
+ destinations.push(`Gramado`); // Adding value to array
  console.log(`Avaliable destinations: ${destinations}`);
- console.log(`Especific destination: ${destinations[1]}`)
+ 
+ //Conditionals
 
- destinations.splice(1,1); //removing value from array
+ const age = 20;
+ const accompanied = true;
+ let passage = true;
+
+ if (age >= 18) {
+      console.log(`Destiny ${destinations[1]} bought.`);
+      destinations.splice(1,1); //removing value from array
+      passage = true;
+ }
+ else {
+     console.log('Client under age.');
+     if(accompanied == true) {
+        console.log(`Client accompanied, destiny ${destinations[1]} bought.`);
+        destinations.splice(1,1); //removing value from array
+        passage = true;
+     } else {
+         console.log(`Client unattended, canno't buy.`)
+         passage = false;
+     }
+ }
+ if(passage == true || accompanied == true) {
+    console.log('boarded passenger')
+ }
+
